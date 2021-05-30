31b479672c0e3862d520fff1e7cbe1f57b8457da
Droege <droege@saenger.io>
2021-05-30 18:40:23 +0200

    initial commit

---
+++ src/main/java/com/droege/javafitnesstracker/JavaFitnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.droege.javafitnesstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFitnessTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFitnessTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/droege/javafitnesstracker/JavaFitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.droege.javafitnesstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFitnessTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
